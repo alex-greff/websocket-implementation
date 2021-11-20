1b4b97956cccd223bd716b16f0e44db44c9e1693
alex-greff <alex.j.greff@gmail.com>
2021-11-19 21:39:51 -0500

    Roughed in initial WebSocketFrame class

---
+++ websocket-client/src/frame.ts
@@ -0,0 +1,306 @@
+import { BitView } from "bit-buffer";
+import { assert } from "tsafe";
+import { WebSocketFrameError } from "./exceptions";
+import * as Utilities from "./utilities";
+
+enum WebSocketFrameOpcode {
+  continuation = 0x0,
+  text = 0x1,
+  binary = 0x2,
+  conn_close = 0x8,
+  ping = 0x9,
+  pong = 0xa,
+}
+
+enum PayloadLenLevel {
+  LEVEL0 = 0,
+  LEVEL1 = 1,
+  LEVEL2 = 2,
+}
+
+enum PayloadLenLevelBits {
+  LEVEL0 = 7,
+  LEVEL1 = 32,
+  LEVEL2 = 64,
+}
+
+const PAYLOAD_LEN_MAX_LVL0 = 125;
+const PAYLOAD_LEN_MAX_LVL1 = (1 << 32) - 1; // 2^32-1
+
+const BITS_LEN_FIN = 1;
+const BITS_LEN_RSV = 3;
+const BITS_LEN_OPCODE = 4;
+const BITS_LEN_MASK = 1;
+
+const BITS_LEN_MASKING_KEY = 4 * 8; // 4 bytes
+
+const MIN_FRAME_BITS_LEN =
+  BITS_LEN_FIN +
+  BITS_LEN_RSV +
+  BITS_LEN_OPCODE +
+  BITS_LEN_MASK +
+  PayloadLenLevelBits.LEVEL0;
+
+const BITS_OFFSET_FIN = 0;
+const BITS_OFFSET_RSV = BITS_LEN_FIN;
+const BITS_OFFSET_OPCODE = BITS_LEN_RSV + BITS_OFFSET_RSV;
+const BITS_OFFSET_MASK = BITS_OFFSET_OPCODE + BITS_LEN_OPCODE;
+const BITS_OFFSET_PAYLOAD = BITS_OFFSET_MASK + BITS_LEN_MASK;
+
+interface WebSocketFrameValues {
+  fin: boolean;
+  opcode: number;
+  mask: boolean;
+  payloadBytesLen: number;
+  maskingKey?: number;
+  payloadData?: Buffer;
+}
+
+export class WebSocketFrame {
+  private fin: boolean;
+  private readonly rsv1: boolean = false;
+  private readonly rsv2: boolean = false;
+  private readonly rsv3: boolean = false;
+  private opcode: number;
+  private mask: boolean;
+  private payloadBytesLen: number;
+  private maskingKey?: number;
+  private payloadData?: Buffer;
+
+  constructor(values: WebSocketFrameValues) {
+    this.validateValues(values);
+
+    this.fin = values.fin;
+    this.opcode = values.opcode;
+    this.mask = values.mask;
+    this.payloadBytesLen = values.payloadBytesLen;
+    this.maskingKey = values.maskingKey;
+    this.payloadData = values.payloadData;
+  }
+
+  private validateValues(values: WebSocketFrameValues): void | never {
+    // TODO: implement
+  }
+
+  private get payloadLenLevel(): PayloadLenLevel {
+    assert(this.payloadBytesLen >= 0);
+
+    if (this.payloadBytesLen <= PAYLOAD_LEN_MAX_LVL0) {
+      return PayloadLenLevel.LEVEL0;
+    } else if (this.payloadBytesLen <= PAYLOAD_LEN_MAX_LVL1) {
+      return PayloadLenLevel.LEVEL1;
+    } else {
+      return PayloadLenLevel.LEVEL2;
+    }
+  }
+
+  private get payloadBitsLength(): number {
+    if (this.payloadLenLevel == PayloadLenLevel.LEVEL0)
+      return PayloadLenLevelBits.LEVEL0;
+    else if (this.payloadLenLevel == PayloadLenLevel.LEVEL1)
+      return PayloadLenLevelBits.LEVEL0 + PayloadLenLevelBits.LEVEL1;
+    else return PayloadLenLevelBits.LEVEL0 + PayloadLenLevelBits.LEVEL2;
+  }
+
+  private get maskingKeyBitsLength(): number {
+    return this.mask ? BITS_LEN_MASKING_KEY : 0;
+  }
+
+  private get maskingKeyBitsOffset(): number {
+    return BITS_OFFSET_PAYLOAD + this.payloadBitsLength;
+  }
+
+  private get payloadBitsLen(): number {
+    return this.payloadBytesLen * 8;
+  }
+
+  private get frameBitsLength(): number {
+    return (
+      BITS_LEN_FIN +
+      BITS_LEN_RSV +
+      BITS_LEN_OPCODE +
+      BITS_LEN_MASK +
+      this.payloadBitsLength +
+      this.maskingKeyBitsLength +
+      this.payloadBitsLen
+    );
+  }
+
+  static fromBuffer(buffer: Buffer): WebSocketFrame {
+    // Ensure that at least everything upto and including
+    // Payload len (7) is there
+    if (buffer.length < MIN_FRAME_BITS_LEN / 8)
+      throw new WebSocketFrameError("Buffer length too small");
+
+    const bitBuffer = new BitView(buffer);
+    let bufferRemainingBits = buffer.length * 8;
+
+    const fin = Utilities.intToBool(
+      bitBuffer.getBits(BITS_OFFSET_FIN, BITS_LEN_FIN, false)
+    );
+    bufferRemainingBits -= BITS_LEN_FIN;
+
+    const rsv = bitBuffer.getBits(BITS_OFFSET_RSV, BITS_LEN_RSV, false);
+    bufferRemainingBits -= BITS_LEN_RSV;
+    if (rsv !== 0) throw new WebSocketFrameError("RSV bits must be zero");
+    const opcode = bitBuffer.getBits(
+      BITS_OFFSET_OPCODE,
+      BITS_LEN_OPCODE,
+      false
+    );
+    // TODO: validate opcode
+
+    const mask = Utilities.intToBool(
+      bitBuffer.getBits(BITS_OFFSET_MASK, BITS_LEN_MASK, false)
+    );
+    bufferRemainingBits -= BITS_LEN_MASK;
+
+    const payloadLenLvl0Val = bitBuffer.getBits(
+      BITS_OFFSET_PAYLOAD,
+      PayloadLenLevelBits.LEVEL0,
+      false
+    );
+    bufferRemainingBits -= PayloadLenLevelBits.LEVEL0;
+
+    let payloadBytesLen: number = 0;
+
+    let payloadLenLevel: PayloadLenLevel;
+    let payloadAdditionalBitsLength;
+    if (payloadLenLvl0Val <= 125) {
+      payloadLenLevel = PayloadLenLevel.LEVEL0;
+      payloadAdditionalBitsLength = 0;
+    } else if (payloadLenLvl0Val === 126) {
+      payloadLenLevel = PayloadLenLevel.LEVEL1;
+      payloadAdditionalBitsLength = PayloadLenLevelBits.LEVEL1;
+    } else if (payloadLenLvl0Val === 127) {
+      payloadLenLevel = PayloadLenLevel.LEVEL2;
+      payloadAdditionalBitsLength = PayloadLenLevelBits.LEVEL2;
+    } else Utilities.notReached();
+
+    if (payloadLenLevel === PayloadLenLevel.LEVEL0)
+      payloadBytesLen = payloadLenLvl0Val;
+    else {
+      if (bufferRemainingBits < payloadAdditionalBitsLength)
+        throw new WebSocketFrameError("Buffer length too small");
+      payloadBytesLen = bitBuffer.getBits(
+        BITS_OFFSET_PAYLOAD + PayloadLenLevelBits.LEVEL0,
+        payloadAdditionalBitsLength
+      );
+      bufferRemainingBits -= payloadAdditionalBitsLength;
+    }
+
+    let maskingKey: number | undefined = undefined;
+    if (mask) {
+      if (bufferRemainingBits < BITS_LEN_MASKING_KEY)
+        throw new WebSocketFrameError("Buffer length too small");
+      maskingKey = bitBuffer.getBits(
+        BITS_OFFSET_PAYLOAD +
+          PayloadLenLevelBits.LEVEL0 +
+          payloadAdditionalBitsLength,
+        BITS_LEN_MASKING_KEY,
+        false
+      );
+      bufferRemainingBits -= BITS_LEN_MASKING_KEY;
+    }
+
+    if (payloadBytesLen * 8 !== bufferRemainingBits)
+      throw new WebSocketFrameError(
+        "Payload length does not match payload bytes"
+      );
+    let payloadData: Buffer | undefined = undefined;
+    if (payloadBytesLen > 0) {
+      payloadData = Buffer.alloc(payloadBytesLen);
+
+      const payloadStartByte =
+        (BITS_OFFSET_PAYLOAD +
+          PayloadLenLevelBits.LEVEL0 +
+          payloadAdditionalBitsLength +
+          BITS_LEN_MASKING_KEY) /
+        8;
+
+      bitBuffer.buffer.copy(
+        payloadData,
+        0,
+        payloadStartByte,
+        payloadStartByte + payloadBytesLen
+      );
+
+      // TODO: do unmasking of payload
+    }
+
+    return new WebSocketFrame({
+      fin,
+      mask,
+      opcode,
+      payloadBytesLen,
+      maskingKey,
+      payloadData,
+    });
+  }
+
+  public toBuffer(): Buffer {
+    const buffer = Buffer.alloc(this.frameBitsLength / 8);
+    const bitBuffer = new BitView(buffer);
+
+    bitBuffer.setBits(
+      BITS_OFFSET_FIN,
+      Utilities.boolToInt(this.fin),
+      BITS_LEN_FIN
+    );
+    bitBuffer.setBits(BITS_OFFSET_RSV, 0, BITS_LEN_RSV);
+    bitBuffer.setBits(BITS_OFFSET_OPCODE, this.opcode, BITS_LEN_OPCODE);
+    bitBuffer.setBits(
+      BITS_OFFSET_MASK,
+      Utilities.boolToInt(this.mask),
+      BITS_LEN_MASK
+    );
+
+    // Set payload length bits
+    if (this.payloadLenLevel === PayloadLenLevel.LEVEL0) {
+      bitBuffer.setBits(
+        BITS_OFFSET_PAYLOAD,
+        this.payloadBytesLen,
+        PayloadLenLevelBits.LEVEL0
+      );
+    } else if (this.payloadLenLevel === PayloadLenLevel.LEVEL1) {
+      bitBuffer.setBits(BITS_OFFSET_PAYLOAD, 126, PayloadLenLevelBits.LEVEL0);
+      bitBuffer.setBits(
+        BITS_OFFSET_PAYLOAD + PayloadLenLevelBits.LEVEL0,
+        this.payloadBytesLen,
+        PayloadLenLevelBits.LEVEL1
+      );
+    } else if (this.payloadLenLevel === PayloadLenLevel.LEVEL2) {
+      bitBuffer.setBits(BITS_OFFSET_PAYLOAD, 127, PayloadLenLevelBits.LEVEL0);
+      bitBuffer.setBits(
+        BITS_OFFSET_PAYLOAD + PayloadLenLevelBits.LEVEL0,
+        this.payloadBytesLen,
+        PayloadLenLevelBits.LEVEL2
+      );
+    } else Utilities.notReached();
+
+    // Set masking key bits, if needed
+    if (this.mask) {
+      assert(this.maskingKey);
+      bitBuffer.setBits(
+        this.maskingKeyBitsOffset,
+        this.maskingKey,
+        this.maskingKeyBitsLength
+      );
+    }
+
+    // Set payload bytes, if needed
+    if (this.payloadData) {
+      assert(this.payloadData.length === this.payloadBytesLen);
+      this.payloadData.copy(
+        bitBuffer.buffer,
+        this.frameBitsLength / 8 - this.payloadBytesLen,
+        0,
+        this.payloadBytesLen
+      );
+
+      // TODO: do masking of payload
+    }
+
+    return bitBuffer.buffer;
+  }
+}
--- websocket-client/src/utilities.ts
+++ websocket-client/src/utilities.ts
@@ -1,12 +1,36 @@
 // Source: https://stackoverflow.com/a/2140723
 /**
  * Performs ASCII case-insensitive equality comparison between the two strings.
- * @param a 
- * @param b 
- * @returns 
+ * @param a String A
+ * @param b String B
+ * @returns Returns true iff a and b case insensitive equal each other.
  */
-export function caseInsensitiveEquals(a: string, b: string) {
-  return typeof a === 'string' && typeof b === 'string'
-        ? a.localeCompare(b, undefined, { sensitivity: 'accent' }) === 0
-        : a === b;
+export function caseInsensitiveEquals(a: string, b: string): boolean {
+  return typeof a === "string" && typeof b === "string"
+    ? a.localeCompare(b, undefined, { sensitivity: "accent" }) === 0
+    : a === b;
+}
+
+/**
+ * A simple function that always throws. Used as an indicator for a piece of
+ * code that should never be reached.
+ */
+export function notReached(): never {
+  throw new Error("Should not be reached");
+}
+
+// Source: https://stackoverflow.com/a/7820695
+/**
+ * Converts a boolean value to its integer representation.
+ */
+export function boolToInt(valBool: boolean): number {
+  return +valBool;
+}
+
+// Source: https://www.samanthaming.com/tidbits/19-2-ways-to-convert-to-boolean/
+/**
+ * Converts an integer value to its boolean representation.
+ */
+export function intToBool(valInt: number): boolean {
+  return Boolean(valInt);
 }
