94086843a094dee12ea2719f1ad99a6e564f53b0
Rumplestomp <chedy9@hotmail.co.uk>
2021-11-23 20:32:13 -0500

    sendPong attempt 1

--- websocket-server/WebSocketServer.py
+++ websocket-server/WebSocketServer.py
@@ -23,6 +23,7 @@ OP_TEXT = 0x1
 OP_BINARY = 0x2
 OP_CLOSE = 0x8
 OP_PING = 0x9
+OP_PONG = 0xa
 
 
 class Server(BaseHTTPRequestHandler):
@@ -71,9 +72,19 @@ class WebSocketConnection:
         if type(data) == str:
             # create new frame and fill out based on length and stuff
             frame = WSFrame(b'')
+            frame.set_opcode(OP_TEXT)
             print(frame)
         elif type(data) == bytes:
             return
+    
+    def _sendPong(self):
+        frame = WSFrame(b'')
+        frame.set_opcode(OP_PONG)
+        payload = frame.get_bytes(True)
+        self.socket.send(payload)
+
+    def _sendClose(self):
+        return
 
     # API method to register websocket message handler
     def onMessage(self, msgHandler):
@@ -108,70 +119,71 @@ class WebSocketConnection:
 
 
 class WSFrame:
-	def __init__(self, bytes):
-		self.bytes = bytes if bytes else b''
-		self.fin = '1' # 1 is True, 0 is False, denotes the final frame as part of one msg
-		self.rsv = '000' # 3 filler bits
-		self.opcode = '0000' # 4 bits, 1 is text, 2 is binary data etc..
-		self.mask = '0' # 1 bit, true if payload is masked (server does not need to mask data)
-		self.payload_len = '0000000' # 7 bits, or 7+16 bits or 7+64 bits
-		# if first 7 bits are 0-125, that is the length of payload
-		# if first 7 bits are 126, we interpret the following 16 bits as the actual payload length
-		# if first 7 bits are 127, we interpret the following 64 bits as the actual payload length
-		self.masking_key = '' # server does not need a masking key if we are not masking yeah
-		self.payload_data = '' # number of bits is 8*the number represented by payload_len
+    def __init__(self, bytes):
+        self.bytes = bytes if bytes else b''
+        self.fin = '1' # 1 is True, 0 is False, denotes the final frame as part of one msg
+        self.rsv = '000' # 3 filler bits
+        self.opcode = '0000' # 4 bits, 1 is text, 2 is binary data etc..
+        self.mask = '0' # 1 bit, true if payload is masked (server does not need to mask data)
+        self.payload_len = '0000000' # 7 bits, or 7+16 bits or 7+64 bits
+        # if first 7 bits are 0-125, that is the length of payload
+        # if first 7 bits are 126, we interpret the following 16 bits as the actual payload length
+        # if first 7 bits are 127, we interpret the following 64 bits as the actual payload length
+        self.masking_key = '' # server does not need a masking key if we are not masking yeah
+        self.payload_data = '' # number of bits is 8*the number represented by payload_len
 
-	def get_frame(self) -> str:
-		"""
-		Returns the bistring representation of the entire frame
-		"""
-		return (
-			self.fin+
-			self.rsv+
-			self.opcode+
-			self.mask+
-			self.payload_len+
-			self.masking_key+
-			self.payload_data
-		)
-	
-	def get_bytes(self, refresh: bool) -> bytes:
-		"""
-		get the byte sequence of the entire frame, refreshing old bytes optionally
-		"""
-		if (not self.bytes) or refresh:
-			self.bytes = bs2b(self.get_frame())
-		return self.bytes
+    def get_frame(self) -> str:
+        """
+        Returns the bistring representation of the entire frame
+        """
 
-	def set_fin(self, fin:bool):
-		self.fin = '1' if fin else '0'
+        return (
+            self.fin+
+            self.rsv+
+            self.opcode+
+            self.mask+
+            self.payload_len+
+            self.masking_key+
+            self.payload_data
+        )
+    
+    def get_bytes(self, refresh: bool) -> bytes:
+        """
+        get the byte sequence of the entire frame, refreshing old bytes optionally
+        """
+        if (not self.bytes) or refresh:
+            self.bytes = bs2b(self.get_frame())
+        return self.bytes
 
-	# opcode: hexadecimal int, sets the opcode variable accordingly
-	def set_opcode(self, opcode:int):
-		s = bin(opcode)[2:] # will be 4 bits if opcode is one hexadecimal char
-		self.opcode = opcode
-	
-	# plen: int, number of bytes that our payload takes up
-	def set_payload_len(self, plen):
-		plen_bits = bin(plen)[2:]
-		if plen <= 125:
-			self.payload_len = plen_bits;
-		elif plen < 65536:
-			# case where we hard code the first 7 bits to be = 126 and append plen_bits
-			prefix = bin(126)[2:]
-			self.payload_len = prefix + plen_bits
-		else:
-			# case where we hard code the first 7 bits to be = 127 and append plen_bits
-			prefix = bin(127)[2:]
-			self.payload_len = prefix + plen_bits
-	
-	def set_payload_data(self, data):
-		if not data:
-			self.payload_data = ''
-			return
-		# if data is text, we convert to bytes, and then convert to bin
-		if type(data) == str:
-			self.payload_data = s2bs(data)
-		# if data is bytes:
-		if type(data) == bytes:
-			self.payload_data = b2bs(data)
+    def set_fin(self, fin:bool):
+        self.fin = '1' if fin else '0'
+
+    # opcode: hexadecimal int, sets the opcode variable accordingly
+    def set_opcode(self, opcode:int):
+        s = bin(opcode)[2:] # will be 4 bits if opcode is one hexadecimal char
+        self.opcode = s
+    
+    # plen: int, number of bytes that our payload takes up
+    def set_payload_len(self, plen):
+        plen_bits = bin(plen)[2:]
+        if plen <= 125:
+            self.payload_len = plen_bits;
+        elif plen < 65536:
+            # case where we hard code the first 7 bits to be = 126 and append plen_bits
+            prefix = bin(126)[2:]
+            self.payload_len = prefix + plen_bits
+        else:
+            # case where we hard code the first 7 bits to be = 127 and append plen_bits
+            prefix = bin(127)[2:]
+            self.payload_len = prefix + plen_bits
+    
+    def set_payload_data(self, data):
+        if not data:
+            self.payload_data = ''
+            return
+        # if data is text, we convert to bytes, and then convert to bin
+        if type(data) == str:
+            self.payload_data = s2bs(data)
+        # if data is bytes:
+        if type(data) == bytes:
+            self.payload_data = b2bs(data)
