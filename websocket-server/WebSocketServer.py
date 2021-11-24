d482f241d57ecdecf4c5457dfe1b76152f3ea906
chedy9 <chedy9@hotmail.co.uk>
2021-11-23 19:56:35 -0500

    added util file and wssFrame class

--- websocket-server/WebSocketServer.py
+++ websocket-server/WebSocketServer.py
@@ -4,6 +4,8 @@ from hashlib import sha1
 import base64
 import struct
 from socket import socket as Socket
+# local imports:
+from wssUtils import s2bs, b2bs, bs2s
 
 # HTTP server listening for requests
 # websocket handshake start
@@ -16,6 +18,9 @@ from socket import socket as Socket
 # websocket and TCP connection closed
 
 GUID = b"258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
+OP_CONTINUATION = 0x0
+OP_TEXT = 0x1
+OP_BINARY = 0x2
 
 
 class Server(BaseHTTPRequestHandler):
@@ -61,7 +66,11 @@ class WebSocketConnection:
 
     # API method to send data over websocket
     def send(self, data):
-        pass
+        # two cases for data: str or bytes
+        if type(data) == str:
+        	pass
+		if type(data) == bytes:
+			pass
 
     # API method to register websocket message handler
     def onMessage(self, msgHandler):
@@ -70,4 +79,65 @@ class WebSocketConnection:
     def _listen(self):
         while True:
             wsMsg = self.socket.recv(512)
+			# TODO: do some parsing and determine which handler to invoke
+			# based on the opcode of the frame
             self.msgHandler(wsMsg)
+
+
+class WSFrame:
+	def __init__(self, bytes):
+		self.bytes = bytes if bytes else b''
+		self.fin = '1' # 1 is True, 0 is False, denotes the final frame as part of one msg
+		self.rsv = '000' # 3 filler bits
+		self.opcode = '0000' # 4 bits, 1 is text, 2 is binary data etc..
+		self.mask = '0' # 1 bit, true if payload is masked (server does not need to mask data)
+		self.payload_len = '0000000' # 7 bits, or 7+16 bits or 7+64 bits
+		# if first 7 bits are 0-125, that is the length of payload
+		# if first 7 bits are 126, we interpret the following 16 bits as the actual payload length
+		# if first 7 bits are 127, we interpret the following 64 bits as the actual payload length
+		self.masking_key = '' # server does not need a masking key if we are not masking yeah
+		self.payload_data = '' # number of bits is 8*the number represented by payload_len
+
+	def get_frame(self):
+		return (
+			self.fin+
+			self.rsv+
+			self.opcode+
+			self.mask+
+			self.payload_len+
+			self.masking_key+
+			self.payload_data
+		)
+
+	def set_fin(self, fin:bool):
+		self.fin = '1' if fin else '0'
+
+	# opcode: hexadecimal int, sets the opcode variable accordingly
+	def set_opcode(self, opcode:int):
+		s = bin(opcode)[2:] # will be 4 bits if opcode is one hexadecimal char
+		self.opcode = opcode
+	
+	# plen: int, number of bytes that our payload takes up
+	def set_payload_len(self, plen):
+		plen_bits = bin(plen)[2:]
+		if plen <= 125:
+			self.payload_len = plen_bits;
+		elif plen < 65536:
+			# case where we hard code the first 7 bits to be = 126 and append plen_bits
+			prefix = bin(126)[2:]
+			self.payload_len = prefix + plen_bits
+		else:
+			# case where we hard code the first 7 bits to be = 127 and append plen_bits
+			prefix = bin(127)[2:]
+			self.payload_len = prefix + plen_bits
+	
+	def set_payload_data(self, data):
+		if not data:
+			self.payload_data = ''
+			return
+		# if data is text, we convert to bytes, and then convert to bin
+		if type(data) == str:
+			self.payload_data = s2bs(data)
+		# if data is bytes:
+		if type(data) == bytes:
+			self.payload_data = b2bs(data)
