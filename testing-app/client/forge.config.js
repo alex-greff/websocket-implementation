fb985de00e2e98450e492d1410accfa720618973
alex-greff <alex.j.greff@gmail.com>
2022-02-04 19:07:57 -0500

    Setup electron forge make targets

--- testing-app/client/forge.config.js
+++ testing-app/client/forge.config.js
@@ -1,30 +1,23 @@
 module.exports = {
-  packagerConfig: {},
   makers: [
     {
-      name: "@electron-forge/maker-squirrel",
-      config: {
-        name: "client",
-      },
-    },
-    {
       name: "@electron-forge/maker-zip",
-      platforms: ["darwin"],
     },
     {
       name: "@electron-forge/maker-deb",
-      config: {},
     },
     {
-      name: "@electron-forge/maker-rpm",
-      config: {},
+      name: "@electron-forge/maker-wix",
+      // config: {
+      //   language: 1033,
+      //   manufacturer: 'd58-lads'
+      // }
     },
   ],
   plugins: [
     [
       "@electron-forge/plugin-webpack",
       {
-        // TODO: add the deployed server's URL here too
         devContentSecurityPolicy:
           "default-src * 'unsafe-eval' 'unsafe-inline'; connect-src 'self' ws://localhost:3051/;",
         mainConfig: "./webpack.electron.main.config.js",
