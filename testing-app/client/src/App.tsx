cb6db3b272a241e84e4b5ede92509caccf61930a
alex-greff <alex.j.greff@gmail.com>
2022-02-07 16:06:17 -0500

    Updated web build to use browser router

--- testing-app/client/src/App.tsx
+++ testing-app/client/src/App.tsx
@@ -1,31 +1,38 @@
 import React, { FunctionComponent } from "react";
 import { ChakraProvider } from "@chakra-ui/react";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter, BrowserRouter, Route, Switch } from "react-router-dom";
 import { PingPongView } from "./views/ping-pong/PingPongView";
 import { ChatView } from "./views/chat/ChatView";
 import { HomeView } from "./views/home/HomeView";
+import isElectron from "is-electron";
 
 /**
  * The root app component.
  */
 export const App: FunctionComponent = (props) => {
+  const AppComponents = (
+    <Switch>
+      <Route exact path="/">
+        <HomeView />
+      </Route>
+
+      <Route path="/ping-pong">
+        <PingPongView />
+      </Route>
+
+      <Route path="/chat">
+        <ChatView />
+      </Route>
+    </Switch>
+  );
+
   return (
     <ChakraProvider>
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <HomeView />
-          </Route>
-
-          <Route path="/ping-pong">
-            <PingPongView />
-          </Route>
-
-          <Route path="/chat">
-            <ChatView />
-          </Route>
-        </Switch>
-      </Router>
+      {isElectron() ? (
+        <HashRouter>{AppComponents}</HashRouter>
+      ) : (
+        <BrowserRouter>{AppComponents}</BrowserRouter>
+      )}
     </ChakraProvider>
   );
 };
