3f01098c5adb0454181208490cd6a866267c7dd9
alex-greff <alex.j.greff@gmail.com>
2022-02-07 16:03:47 -0500

    Added download links for linux and windows electron builds

--- testing-app/client/src/components/BrowserAlert.tsx
+++ testing-app/client/src/components/BrowserAlert.tsx
@@ -1,4 +1,12 @@
-import { Alert, AlertDescription, AlertIcon, AlertTitle } from "@chakra-ui/react";
+import {
+  Alert,
+  AlertDescription,
+  AlertIcon,
+  AlertTitle,
+  Text,
+  Link,
+  Flex,
+} from "@chakra-ui/react";
 import { FunctionComponent } from "react";
 
 /**
@@ -14,15 +22,46 @@ export const BrowserAlert: FunctionComponent = (props) => {
       justifyContent="center"
       textAlign="center"
     >
-      <AlertIcon boxSize='40px' mr={0} />
-      <AlertTitle mt={4} mb={1} fontSize='lg'>
+      <AlertIcon boxSize="40px" mr={0} />
+      <AlertTitle mt={4} mb={1} fontSize="lg">
         Web Browser Detected!
       </AlertTitle>
       <AlertDescription maxWidth="md">
-        Due to limitations in the browser preventing us from accessing the TLS
-        layer directly, the implemented Websocket client cannot be run on
-        the browser. As such, only the Websocket client provided by the WebAPI
-        is able to be used.
+        <Text marginBottom="5">
+          Due to limitations in the browser preventing us from accessing the TLS
+          layer directly, the implemented Websocket client cannot be run on the
+          browser. As such, only the Websocket client provided by the WebAPI is
+          able to be used.
+        </Text>
+
+        <Text>
+          The Electron builds can be downloaded below which does use our
+          Websocket client. To use, unzip and run the client/client.exe files,
+          no installation is necessary.
+        </Text>
+
+        <Flex
+          width="100%"
+          flexDirection="row"
+          gap="2"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Link
+            color="blue.800"
+            href={`${process.env.PUBLIC_URL}/desktop-builds/demo-linux-x64.zip`}
+            target="_blank"
+          >
+            demo.zip (Linux)
+          </Link>
+          <Link
+            color="blue.800"
+            href={`${process.env.PUBLIC_URL}/desktop-builds/demo-windows-x64.zip`}
+            target="_blank"
+          >
+            demo.zip (Windows)
+          </Link>
+        </Flex>
       </AlertDescription>
     </Alert>
   );
